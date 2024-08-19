f2824900f4bfba5153ffeab5898d371446b5b5db
Yash Kumar <150715625+kyashf@users.noreply.github.com>
2024-08-19 13:46:32 +0530

    Update

---
+++ mathsProblem01.py
@@ -0,0 +1,12 @@
+# Maths weird problem *This loop never end*
+
+num = int(input("Enter Number: "))
+
+while num>0:
+	if num%2==0:
+		num = num/2
+	else:
+		num = (num*3)+1
+print(num)
+
+# Firoziyash
