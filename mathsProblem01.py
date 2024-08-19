26b16d55b0849639ea362a8474835ed3e32adfae
Yash Kumar <150715625+kyashf@users.noreply.github.com>
2024-08-19 13:51:14 +0530

    Update mathsProblem01.py

--- mathsProblem01.py
+++ mathsProblem01.py
@@ -1,12 +1,11 @@
 # Maths weird problem *This loop never end*
 
-num = int(input("Enter Number: "))
-
-while num>0:
-	if num%2==0:
-		num = num/2
-	else:
-		num = (num*3)+1
+num = int(input("Enter the Natural Number: "))
+while num > 0:
+  if num%2==0:
+    num = num/2
+  else:
+    num = num*3+1
 print(num)
 
 # Firoziyash
